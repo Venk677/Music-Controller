9cbce345ea64d5bc4af655fe6e6693fa87bad864
Venk677 <venkatesh.vk677@gmail.com>
2020-12-07 02:03:08 +0530

    Configured API for creating a room

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,8 @@
-from .views import RoomView
+from .views import RoomView,CreateRoomView
 from django.urls import path
 
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
